956d2cfa5a05452b5cf690281fd137d0d0d77427
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2020-09-27 02:25:37 +0300

    Add files via upload

---
+++ sr_arfm2.py
@@ -0,0 +1,16 @@
+mas = [-1, 6, 8, 2, 4, 3, 9, 0, 5, 1]
+
+n = len(mas)
+print("Amount of elements: ")
+print(n)
+print("")
+
+for summ in range (0, len(mas))
+
+summ = mas[0] + mas[i]
+print("Sum of elelments: ")
+print(summ)
+
+
+average = summ / n
+print(average)

34ec69251f0e1340cd57633579d20244ebf0cea0
danielsummer044 <danielsummer044@gmail.com>
2020-10-04 11:16:46 +0300

    Finished average.py

--- average.py
+++ average.py
@@ -1,7 +1,8 @@
 mas = [-1, 6, 8, 2, 4, 3, 9, 0, 5, 1]
+# mas = [1,2,3]
 summ = 0
 
-for i in range (1, len(mas)):
+for i in range (0, len(mas)):
 	summ = summ + mas[i]
 
 print("Sum of elements in array: ")
@@ -18,7 +19,14 @@ print("Average of array: ")
 print(avg)
 
 
+# find closest to avg
+min_deviation = abs(mas[0] - avg)
+closest = mas[0]
 
 for i in range (1, len(mas)):
-	if mas[i] == summ:
-print(mas[i])
+	deviation = abs(mas[i] - avg)
+	if min_deviation > deviation:
+		min_deviation = deviation
+		closest = mas[i]
+	
+print(f"closest to avg: {closest}")

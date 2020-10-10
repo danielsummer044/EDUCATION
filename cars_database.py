623607093e143b1d071aa5365edebb4b745b0722
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2020-10-11 01:43:51 +0300

    Add files via upload

--- cars_database.py
+++ cars_database.py
@@ -1,12 +1,14 @@
 FILENAME='cars.csv'
 
 def read_all_data(filename):
+
 	f = open(filename, 'r')
 	data = f.readlines()
 	return data[1:]
 
 
 def get_cars_less_than(cars_data, price):
+
 	cars = []
 	for car_info in cars_data:
 		if int(car_info.split(';')[2]) < price:
@@ -15,7 +17,9 @@ def get_cars_less_than(cars_data, price):
 
 
 def search_cars_by_year(cars_data, year):
+
 	"""Search all newest cars, starting from given year"""
+
 	cars = []
 	for car_year in cars_data:
 		if int(car_year.split(';')[1]) >= year:
@@ -24,7 +28,9 @@ def search_cars_by_year(cars_data, year):
 
 
 def sort_cars(cars_data, order):
+
 	""" order - ASC, DESC """
+
 	for i in range(len(cars_data)):
 		for j in range(len(cars_data)):
 			if order == 'ASC':
@@ -36,14 +42,67 @@ def sort_cars(cars_data, order):
 	return cars_data
 
 
+
+
+
+
+#DZ 1
+
+def sort_cars_by_year(cars_data, year):
+
+	for i in range(len(cars_data)):
+		for j in range(len(cars_data)):
+
+				if int(cars_data[i].split(';')[1]) < int(cars_data[j].split(';')[1]):
+					cars_data[i], cars_data[j] = cars_data[j], cars_data[i]
+	return cars_data
+
+
+
+
+# DZ 3
+
+import csv 
+
+with open("cars.csv", "r") as csvfile:
+
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		print(row)
+
+with open("cars.csv", "a") as csvfile:
+
+	fieldnames = ["Model (marka)", "year", "price"]
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writerow({"Model (marka)": "Kamaz", "year": 2002, "price": 6000})
+
+exit()
+
+
+
 def main():
+
 	all_data = read_all_data(FILENAME)
 	cars = get_cars_less_than(all_data, 20000)
-	#print(cars)
+
+	print(cars)
 
 	cars = search_cars_by_year(all_data, 2019)
-	#print(cars)
+
+	print(cars)
+
 	cars = sort_cars(all_data, 'DESC')
 	print(cars)
+
+
+
+
+
+
+# DZ 1
+
+	cars_by_year = sort_cars_by_year(all_data, 2019)
+	print(cars_by_year)
+
 
 main()

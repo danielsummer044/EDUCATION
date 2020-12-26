e8791973cec283d3df06d9185e75a68006b1b47c
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2020-12-27 01:55:58 +0200

    Add files via upload

--- suma_cifr.py
+++ suma_cifr.py
@@ -1,9 +1,28 @@
-n = 75312
- 
-sum_cifr = 0
+a = [11, -23, 13, 45, 51, -96, 69, 71, 19, 54, 99]
 
-while n > 0:
-	sum_cifr = sum_cifr + n % 10 
-	n = n // 10
+x = a[4] // 10 #persha
+y = a[4] % 2 #druga
 
-print(sum_cifr)
+z = a[4] / 10 #perevirka
+
+h = x + y
+
+print(x)
+print(y)
+print(z)
+
+if h > 10:
+	print(h)
+
+
+
+
+"""
+summ = 0
+
+for i in range(len(a)):
+	x = a[i] // 10 # x = ( ( a[i] // 10 ) + ( a[i] % 2) )
+	y = a[i] % 2
+	z = x + y
+print(z)
+"""

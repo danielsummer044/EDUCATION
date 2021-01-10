f5555242686ebbee880082c78a28d52d84b24db9
danielsummer044 <danielsummer044@gmail.com>
2021-01-10 11:40:35 +0200

    Fix chisla_mij_indexami_masivi.py

--- chisla_mij_indexami_masivi.py
+++ chisla_mij_indexami_masivi.py
@@ -1,34 +1,20 @@
 mas = [1, 22, -3, 4, 9, 7, -24]
 
-"""
-mas[0] = 0
-mas[1] = 0
-mas[3] = 0
-mas[5] = 0 
-mas[6] = 0
-print(mas)
-"""
-#mas[2] = nothing 
-#mas[4] = nothing
+#mas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# x = 3, y = 7 => mas = [1, 2, 3, 0, 0, 0, 7, 8, 9, 10]
+#mas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-"""
-for i in range(len(mas)):
-	mas[i] = 0
+x = 22
+y = -24
+index_of_x = mas.index(x)
+index_of_y = mas.index(y)
 
-	mas[2] = -3
-	mas[4] = 9
-
-print(mas)
-"""
-
-
-x = -3
-y = 9
-
-for i in range(len(mas)):
-	if mas[i] == x or mas[i] == y:
-		mas[i] != 0
-	else:
+if index_of_x < index_of_y:
+	for i in range(index_of_x+1, index_of_y):
 		mas[i] = 0
-print(mas)
 
+else:
+	for i in range(index_of_y+1, index_of_x):
+		mas[i] = 0
+
+print(mas)

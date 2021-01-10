b0ace247e3c1b205f77277f7430eea29c445d302
danielsummer044 <danielsummer044@gmail.com>
2021-01-10 12:08:26 +0200

    Fix suma_cifr.py

--- suma_cifr.py
+++ suma_cifr.py
@@ -1,28 +1,27 @@
-a = [11, -23, 13, 45, 51, -96, 69, 71, 19, 54, 99]
+#a = [11, -23, 13, 45, 51, -96, 69, 71, 19, 54, 99]
 
-x = a[4] // 10 #persha
-y = a[4] % 2 #druga
+a = 123
 
-z = a[4] / 10 #perevirka
+def suma_cifr(a):
+	summ = 0
+	while a > 0:
+		cifra = a % 10
+		a = a // 10
+		summ += cifra
+	return summ
 
-h = x + y
-
-print(x)
-print(y)
-print(z)
-
-if h > 10:
-	print(h)
-
-
-
+print(suma_cifr(a))
 
 """
-summ = 0
+d = a % 10
 
-for i in range(len(a)):
-	x = a[i] // 10 # x = ( ( a[i] // 10 ) + ( a[i] % 2) )
-	y = a[i] % 2
-	z = x + y
-print(z)
+a = a // 10
+c = a % 10
+
+a = a // 10
+b = a % 10
 """
+
+
+#summ = d + b + c
+#print(summ)

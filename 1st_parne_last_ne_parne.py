45118904f72267d960216a2ac84c52b974ea8bb8
danielsummer044 <danielsummer044@gmail.com>
2021-01-17 12:06:05 +0200

    Add fixed homework

--- 1st_parne_last_ne_parne.py
+++ 1st_parne_last_ne_parne.py
@@ -1,39 +1,32 @@
-mas = [7, 1, 13, 4, 17, 12, 16, 18, 3, 6, 5]
+mas = [7, 1, 13, 4, 17, 12, 16, 18, 3, 6, 5, 7]
 
-parne = 0
+index_parne = 0
+parne = False
 for i in range(len(mas)):
-	while parne == 1:
-		mas[i] % 2 == 0
-#print("parne")
-print(mas[i])
-"""
-x = mas.index(mas[i])
-print("Index of parne: ") # index of parne
-print(x)
-"""
-"""
-ne_parne = 0
-for i in range(len(mas)):
+	if mas[i] % 2 == 0:
+		index_parne = i
+		parne = True
+	if parne == True:
+		break
+
+#print(index_parne)
+
+
+index_neparne = 0
+neparne = False
+for i in range(len(mas)-1, 0, -1):
 	if mas[i] % 2 != 0:
-		ne_parne = mas[i]
-		#print(ne_parne)
+		index_neparne = i
+		neparne = True
+	if neparne == True:
+		break
 
-mas2 = []
-mas2.append(ne_parne)
-#print(mas2[0])
+#print(index_neparne)
 
-y = mas2.index(ne_parne)
-print("Index of ne parne: ")
-print(y) # index of ne parne
 
-z = 0
-for i in range(len(mas)):
-	if mas2[0] == mas[i]:
-		z = mas[i]
-		print(mas.index(z))
-"""
-
-mas = [5, 7, 1, 13, 4, 17, 12, 16, 18, 3, 6, 5, 7]
-for i in range(mas[-1], mas[0]):
-	mas[i] = 0
-print(mas)
+if index_parne < index_neparne:
+	for i in range(index_parne+1, index_neparne):
+		print(mas[i])
+else:
+	for i in range(index_neparne+1, index_parne):
+		print(mas[i])
--- suma_cifr_bilshe_10.py
+++ suma_cifr_bilshe_10.py
@@ -1,12 +1,21 @@
-a = [11, -23, 13, 45, 51, -96, 69, 71, 19, 54, 99]
+"""Module designed to print number from list which has sum of digits > 10"""
 
-summ = 0
+def suma_cifr(a):
+	summ = 0
+	while a > 0:
+		cifra = a % 10
+		a = a // 10
+		summ += cifra
+	return summ
 
-for i in range(len(a)):
 
-	cifra = a[i] % 10
-	druga_cifra = a[i] // 10
-	summ = cifra + druga_cifra
+def main():
+	a = [11, -23, 113, 45, 51, -96, 69, 71, 19, 54, 99]
 
-	if summ >= 10:
-		print(summ)
+	for number in a:  # for i in range(len(a)): number = a[i]
+		if suma_cifr(number) > 10:
+			print(number)
+
+
+if __name__ == '__main__':
+	main()

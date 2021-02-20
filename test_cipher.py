3c9bfb26a534e0a51cd13982a6643f98c807d396
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2021-02-21 00:23:55 +0200

    Add files via upload

--- test_cipher.py
+++ test_cipher.py
@@ -1,4 +1,4 @@
-from cipher import get_new_letter
+from cipher import get_new_letter, encode
 from string import ascii_lowercase, ascii_uppercase
 
 
@@ -6,17 +6,34 @@ def test_get_new_letter():
     """
     'abc....xyz', 'a', step = 1, => b
     """
+
     assert get_new_letter(ascii_lowercase, 'a', 1) == 'b'
     assert get_new_letter(ascii_lowercase, 'z', 1) == 'a'
     assert get_new_letter(ascii_lowercase, 'a', -1) == 'z'
+
+    assert get_new_letter(ascii_uppercase, 'A', 1) == 'B'
+    assert get_new_letter(ascii_uppercase, 'Z', 1) == 'A'
+    assert get_new_letter(ascii_uppercase, 'A', -1) == 'Z'
+
     print('test passed')
 
 
 def test_get_new_letter_step():
+
     assert get_new_letter(ascii_lowercase, 'd', -3) == 'a'
     assert get_new_letter(ascii_lowercase, 'a', 3) == 'd'
+
+    assert get_new_letter(ascii_uppercase, 'D', -3) == 'A'
+    assert get_new_letter(ascii_uppercase, 'A', 3) == 'D'
+
+    print('test passed')
+
+def test_encode():
+
+    assert encode('Hello', 1) == 'Ifasdaadfdafmp'
     print('test passed')
 
 
 test_get_new_letter()
 test_get_new_letter_step()
+test_encode()

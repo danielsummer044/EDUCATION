4d675639c51b19cfb86ea219cdf12d01782dd5c2
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2021-03-28 04:13:49 +0300

    Add files via upload

--- center_string.py
+++ center_string.py
@@ -4,17 +4,34 @@ space_width = '-' * 10
 print(space_width + string + space_width)
 """
 
+"""
 string = 'Hello World!'
+string_len = len(string)
 
-width = 3
+width = 40
 space = '-'
 space_width = space * width
 
-left_pos = (width // 2)
+left_pos = (width // 2) - string_len
 #center_pos = (width // 2) + 1
 right_pos = (width // 2)
 
 print(left_pos * space + string + right_pos * space )
 #for i in range(center_pos, width + 1):
 #    print(i)
+"""
+
+"""
+string = 'Hello World!'
+new_string = string.center(40, '-')
+print(new_string)
+"""
+
+string = 'Hello World!'
+width = 40
+space = '-'
+left_right = width - len(string)
+left = left_right // 2
+right = left
+print(left * space + string + right * space)
 

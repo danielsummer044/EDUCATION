de13dc32b98fd06baddda8d94fa902bec9800250
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2021-04-17 23:31:44 +0300

    Add files via upload

---
+++ letters_vowels_upper.py
@@ -0,0 +1,11 @@
+from string import ascii_uppercase
+
+def vowels_uppercase_letters(letter):
+    vowels = "AEIOUYaeiouy"
+    for word in text:
+        for letter in word:
+            if letter in vowels and letter in ascii_uppercase:
+                print(letter)
+
+text = "Hello, my name is DANIEL"
+vowels_uppercase_letters(text)

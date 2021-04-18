7150289c6e171a0563fd830e3eeec07502ce1dd5
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2021-04-18 04:24:44 +0300

    ADDED MESSAGES

--- XO.py
+++ XO.py
@@ -1,16 +1,11 @@
 import random
 
-
 board = [[' ', ' ', ' '],
          [' ', ' ', ' '],
          [' ', ' ', ' ']]
 
-
-
 letters = 'ABC'
 digits = '012'
-
-
 
 def print_board(board):
 
@@ -18,8 +13,6 @@ def print_board(board):
     for index, row in enumerate(board):
         print(f'{index} {row}')
 print_board(board)
-
-
 
 def player_move():
 
@@ -34,8 +27,6 @@ def player_move():
         else:
             print("Клітинка зайнята")
 
-
-
 def computer_move():
 
     move_incorrect = True
@@ -48,16 +39,12 @@ def computer_move():
             board[row][column] = 'O'
             move_incorrect = False
 
-
-
 def is_board_full(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == ' ':
                 return False
     return True
-
-
 
 def is_player_win(symbol, board):
 
@@ -69,8 +56,6 @@ def is_player_win(symbol, board):
         if win:
             return True
 
-
-
     for j in range(len(board[0])):
         win = True
         for i in range(len(board)):
@@ -79,8 +64,6 @@ def is_player_win(symbol, board):
         if win:
             return True
 
-
-
     win = True
     for i in range(len(board)):
         if board[i][i] != symbol:
@@ -88,16 +71,12 @@ def is_player_win(symbol, board):
     if win:
         return True
 
-
-
     win = True
     for i in range(len(board)):
         if board[i][len(board) - 1 - i] != symbol:
             win = False
     if win:
         return True
-
-
 
 def is_computer_win(symbol, board):
 
@@ -109,25 +88,33 @@ def is_computer_win(symbol, board):
             if win:
                 return True
 
-
-
 def game_tie(board):
+
     return is_board_full(board) and not (is_player_win('X', board) or is_player_win('O', board))
 
 def messages():
 
-    if is_player_win():
-        print("PLAYER WON!")
-    if is_computer_win():
-        print("COMPUTER WON!")
+    if is_player_win('X', board):
 
+        print()
+        print("▼▲     ▼▲     ▼▲     ▼▲     ▼▲     ▼▲     ▼▲     ▼▲")
+        print("◀▶      P L A Y E R     (* ᴗ *)     W O N !      ◀▶")
+        print("▲▼     ▲▼     ▲▼     ▲▼     ▲▼     ▲▼     ▲▼     ▲▼")
 
+        return is_player_win('X', board)
+
+    if is_computer_win('O', board):
+
+        print()
+        print("▼▲     ▼▲     ▼▲     ▼▲     ▼▲     ▼▲     ▼▲     ▼▲")
+        print("◀▶      C O M P U T E R   (x ᴖ x)   W O N !      ◀▶")
+        print("▲▼     ▲▼     ▲▼     ▲▼     ▲▼     ▲▼     ▲▼     ▲▼")
+
+        return is_computer_win('X', board) # WHY X ???
 
 def game_over(board):
 
-    return is_player_win('X', board) or is_player_win('O', board) or is_board_full(board)
-
-
+    return is_player_win('X', board) or is_player_win('O', board) or is_board_full(board) #WHY O ???
 
 def play():
 
@@ -135,13 +122,13 @@ def play():
         player_move()
         print_board(board)
         if game_over(board):
+            messages()
             break
 
         computer_move()
         print_board(board)
         if game_over(board):
+            messages()
             break
-
-
 
 play()

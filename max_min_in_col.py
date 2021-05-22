3ae8c835c28ce81d0b5e5d31ca6fedd08a3ac76a
danielsummer044 <69406087+danielsummer044@users.noreply.github.com>
2021-05-23 02:08:18 +0300

    Find the maximum and minimum element in a column of a 2D matrix.

---
+++ max_min_in_col.py
@@ -0,0 +1,62 @@
+array = [[1, 2, 3],
+         [4, 5, 6],
+         [0, 8, 9]]
+"""
+min_elem = array[0][0]
+max_elem = array[0][0]
+
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][0] < min_elem:
+            min_elem = array[i][0]
+        if array[i][0] > max_elem:
+            max_elem = array[i][0]
+
+print(min_elem)
+print(max_elem)
+"""
+
+"""
+min_elem = array[0][1]
+max_elem = array[0][1]
+
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][1] < min_elem:
+            min_elem = array[i][1]
+        if array[i][1] > max_elem:
+            max_elem = array[i][1]
+
+print(min_elem)
+print(max_elem)
+"""
+
+"""
+min_elem = array[0][2]
+max_elem = array[0][2]
+
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][2] < min_elem:
+            min_elem = array[i][2]
+        if array[i][2] > max_elem:
+            max_elem = array[i][2]
+
+print(min_elem)
+print(max_elem)
+"""
+
+
+for i in range(len(array)):
+    min_elem = array[0][i]
+    max_elem = array[0][i]
+    for j in range(len(array[i])):
+
+        if array[j][i] < min_elem:
+            min_elem = array[j][i]
+        if array[j][i] > max_elem:
+            max_elem = array[j][i]
+
+    print("-" * 30)
+    print("|", i+1, "col.", "|", "min :", min_elem, "|", "max :", max_elem, "|")
+print("-" * 30)
